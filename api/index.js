381074b8f4b300815cb66ba7d4b02c82d174e860
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2021-02-02 14:01:45 +0530

    add a eventSwitch func to handle events received

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+const event = require("../lib/eventSwitch.js");
+
 // The main, exported, function of the endpoint,
 // dealing with the request and subsequent response
 module.exports = async (req, res) => {
@@ -8,8 +10,11 @@ module.exports = async (req, res) => {
     const gh_sig_256 = req.headers["x-hub-signature-256"];
 
     if (method === "POST") {
-        res.status(200).send()
+        event.eventSwitch(gh_event, body).then((Msg) => {
+            console.log(Msg);
+        });
+        res.status(200).send();
     } else {
-        res.status(403).send()
+        res.status(403).send();
     }
 };
