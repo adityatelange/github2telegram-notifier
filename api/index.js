c928ebc95f2d69c9f439e880b2338fe645b377ce
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2021-02-02 15:46:35 +0530

    secure webhook - add webhook verification via X-Hub-Signature-256 - ref: https://docs.github.com/en/developers/webhooks-and-events/securing-your-webhooks

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const event = require("../lib/eventSwitch.js");
 const sendMsg = require("../lib/sendMsg.js");
+const verifyWebhook = require("../lib/verifyWebhook.js");
 
 // The main, exported, function of the endpoint,
 // dealing with the request and subsequent response
@@ -10,7 +11,7 @@ module.exports = async (req, res) => {
     const gh_sig = req.headers["x-hub-signature"];
     const gh_sig_256 = req.headers["x-hub-signature-256"];
 
-    if (method === "POST") {
+    if ((method === "POST") && verifyWebhook.verifyWebhook(body, gh_sig_256)) {
         event.eventSwitch(gh_event, body).then((Msg) => {
             console.log(Msg);
             sendMsg.sendMsg(Msg)
