75f5081194c513427be851c4d88bc9d162f98c25
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2021-02-02 13:57:52 +0530

    store delivery headers
    https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#delivery-headers

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@
 module.exports = async (req, res) => {
     const body = req.body;
     const method = req.method;
+    const gh_event = req.headers["x-github-event"];
+    const gh_sig = req.headers["x-hub-signature"];
+    const gh_sig_256 = req.headers["x-hub-signature-256"];
 
     if (method === "POST") {
         res.status(200).send()
